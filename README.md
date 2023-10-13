f083f20c2d223178ca2e2d33ffbcb58206cb2ee5
Bloodiko <bastian.jesuiter@gmail.com>
2023-10-13 17:02:55 +0200

    add debug code + Document in readme

--- README.md
+++ README.md
@@ -1,5 +1,41 @@
 # bun-pub_sub-test
 
+
+This is a test of the pub/sub functionality of bun. 
+
+Its just the example from the bun docs, but with a few extra lines to make it work.
+
+I added some console logs to the index.ts file to understand how it works. 
+
+Try it in web console with 
+
+```js
+let ws1 = new WebSocket("ws://localhost:3000");
+let ws2 = new WebSocket("ws://localhost:3000");
+let ws3 = new WebSocket("ws://localhost:3000");
+ws1.onmessage = (e) => console.log(`Websocket 1: ${e.data}`);
+ws2.onmessage = (e) => console.log(`Websocket 2: ${e.data}`);
+ws3.onmessage = (e) => console.log(`Websocket 2: ${e.data}`);
+
+setTimeout(() => ws1.send("Hello from Websocket1"), 1000);
+setTimeout(() => ws2.send("Hello from Websocket2"), 2000);
+setTimeout(() => ws3.send("Hello from Websocket3"), 3000);
+
+```
+
+
+# Findings: 
+
+* You **must** be subscribed to a channel to be able to publish via `ws.publish(channel, msg)`. 
+* Your own Websocket will **not** receive the message you publish via `ws.publish(channel, msg)`. 
+  * Make sure to display the message for yourself as well if you want to see it.
+* To publish a message without being subscribed, use `server.publish(channel, msg)`. (I think this might even be a bug, but its good that its there.)
+* Use `server.publish(channel, msg)` to send a message to all subscribers of a channel. 
+  * This works even if you are not subscribed to the channel yourself.
+
+
+-------
+
 To install dependencies:
 
 ```bash
--- index.ts
+++ index.ts
@@ -1,1 +1,60 @@
-console.log("Hello via Bun!");
+const usernames = ["Alice", "Bob", "Charlie", "Dave", "Eve", "Frank"];
+
+let wsCounter = 0;
+
+const server = Bun.serve<{ username: string }>({
+  fetch(req, server) {
+    const success = server.upgrade(req, {
+      data: {
+        username:
+          usernames[wsCounter % usernames.length] +
+          Math.floor(wsCounter++ / usernames.length),
+      },
+    });
+    if (success) return undefined;
+
+    return new Response("Hello world");
+  },
+  websocket: {
+    open(ws) {
+      const msg = `${ws.data.username} has entered the chat`;
+
+      ws.send(`This websocket got the Username: ${ws.data.username}`);
+
+      console.log(`Websocket opened with user ${ws.data.username}`);
+      ws.subscribe("the-group-chat");
+      if (ws.data.username === "Charlie0") {
+        ws.unsubscribe("the-group-chat"); // Explicitly unsubscribe to test what happens when a user is not subscribed and sends a message
+      }
+      ws.publish("the-group-chat", msg);
+      server.publish("the-group-chat", `ServerPublished: ${msg}`);
+      console.log(`Websocket opened with user ${ws.data.username} published.`);
+    },
+    message(ws, message) {
+      // the server re-broadcasts incoming messages to everyone
+      console.log(
+        `Message in ws with user ${ws.data.username} recieved: `,
+        message
+      );
+      ws.publish("the-group-chat", `${ws.data.username}: ${message}`);
+      server.publish(
+        "the-group-chat",
+        `ServerPublished: ${ws.data.username}: ${message}`
+      );
+      console.log(
+        `Message from ws with user ${ws.data.username} published: `,
+        message
+      );
+    },
+    close(ws) {
+      const msg = `${ws.data.username} has left the chat`;
+
+      console.log("Websocket Closed with message: ", msg);
+      ws.publish("the-group-chat", msg);
+      server.publish("the-group-chat", `ServerPublished: ${msg}`);
+      ws.unsubscribe("the-group-chat");
+    },
+  },
+});
+
+console.log(`Listening on ${server.hostname}:${server.port}`);
